c8d03633d70057c403fcbbab5d2b70961c8b3fa6
reduckted <reduckted@outlook.com>
2021-03-23 21:41:14 +1000

    Ensured the search box is focused when the dialog is opened.

---
+++ source/ProjectFilter/UI/AttachedProperties/FocusWhenVisible.cs
@@ -0,0 +1,71 @@
+using Microsoft.VisualStudio;
+using System;
+using System.Windows;
+
+
+namespace ProjectFilter.UI.AttachedProperties {
+
+    public static class FocusWhenVisible {
+
+        public static readonly DependencyProperty EnabledProperty = DependencyProperty.RegisterAttached(
+            "Enabled",
+            typeof(bool),
+            typeof(FocusWhenVisible),
+            new PropertyMetadata(false, OnEnabledChanged)
+        );
+
+
+        public static bool GetEnabled(DependencyObject obj) {
+            if (obj is null) {
+                throw new ArgumentNullException(nameof(obj));
+            }
+
+            return (bool)obj.GetValue(EnabledProperty);
+        }
+
+
+        public static void SetEnabled(DependencyObject obj, bool value) {
+            if (obj is null) {
+                throw new ArgumentNullException(nameof(obj));
+            }
+
+            obj.SetValue(EnabledProperty, value);
+        }
+
+
+        private static void OnEnabledChanged(DependencyObject d, DependencyPropertyChangedEventArgs e) {
+            UIElement? target;
+
+
+            target = d as UIElement;
+
+            if (target != null) {
+                if ((bool)e.OldValue) {
+                    target.IsVisibleChanged -= OnIsVisibleChanged;
+                }
+
+                if ((bool)e.NewValue) {
+                    target.IsVisibleChanged += OnIsVisibleChanged;
+                }
+            }
+        }
+
+
+        private static void OnIsVisibleChanged(object sender, DependencyPropertyChangedEventArgs e) {
+            UIElement? target;
+
+
+            target = sender as UIElement;
+
+            if (target != null && target.IsVisible) {
+                try {
+                    target.Focus();
+                } catch (Exception ex) when (!ErrorHandler.IsCriticalException(ex)) {
+                    System.Diagnostics.Debug.WriteLine(ex);
+                }
+            }
+        }
+
+    }
+
+}
